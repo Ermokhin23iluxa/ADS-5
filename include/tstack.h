37af26b38a0dc35ae329f9109f8087e3082a0213
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-04-02 19:11:42 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,26 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T arr[size] = { 0 };
+  int top;
+public:
+  TStack() :top(-1) {}
+  void push(T value) {
+    arr[++top] = value;
+  }
+  T get()const {
+  return arr[top];
+  }
+  T pop() {
+    return arr[top--];
+  }
+  bool isEmpty()const {
+    return top == -1;
+  }
+  bool isFull()const {
+    return top == size - 1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
