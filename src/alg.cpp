f7aa999161a39557eb1768c156ed9f5d8eb80f9b
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-04-09 10:59:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,154 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
+TStack <char, 100> charstack;
+TStack <char, 100> vixod;
+TStack <int, 100> intstack;
+int getPrior(char inf) {
+    switch (inf) {
+    case '(':
+        return 0;
+        break;
+    case ')':
+        return 1;
+        break;
+    case'+':
+        return 2;
+        break;
+    case'-':
+        return 2;
+        break;
+    case'*':
+        return 3;
+        break;
+    case'/':
+        return 3;
+        break;
+    default:
+        return -1;
+        break;
+    }
+}
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+    int count = 0, count1 = 0, count2 = 0;
+    std::string res;
+    std::string exit;
+    for (auto c : inf) {
+        count += 1;
+    }
+    for (int i = 0; i < count; ++i) {
+        int prior = getPrior(inf[i]);
+        char op = inf[i];
+        if (prior == -1) {
+            vixod.push(inf[i]);
+        } else {
+            if (prior > getPrior(charstack.peek()) || charstack.isEmpty()) {
+                charstack.push(op);
+            } else if (prior == 0) {
+                charstack.push(op);
+            } else if (op == ')') {
+                while (prior <= getPrior(charstack.peek())) {
+                    if (charstack.peek() != '(' && charstack.peek() != ')') {
+                        vixod.push(charstack.peek());
+                    }
+                    charstack.pop();
+                }
+                charstack.pop();
+            } else {
+                while (prior <= getPrior(charstack.peek())) {
+                    if (charstack.peek() != '(' && charstack.peek() != ')') {
+                        vixod.push(charstack.peek());
+                    }
+                    charstack.pop();
+                }
+                charstack.push(op);
+            }
+        }
+    }
+    while (!charstack.isEmpty()) {
+        if (charstack.peek() != '(' && charstack.peek() != ')') {
+            vixod.push(charstack.peek());
+        }
+        charstack.pop();
+    }
+    res.resize(count);
+    auto it = res.rbegin();
+    while (!vixod.isEmpty()) {
+        *it = vixod.peek();
+        vixod.pop();
+        ++it;
+        count1 += 1;
+    }
+    for (auto a : res) {
+        if (res.find(' ')) {
+            count2 += 1;
+        }
+    }
+    int get = count2 - count1;
+    for (int i = get; i < count; ++i) {
+        char ex = res[i];
+        exit.push_back(ex);
+        exit.push_back(' ');
+    }
+    exit.pop_back();
+    return exit;
 }
 
+int operat(char oper) {
+    int a = intstack.peek();
+    intstack.pop();
+    int b = intstack.peek();
+    intstack.pop();
+    switch (oper) {
+    case '-':
+        return b - a;
+        break;
+    case '+':
+        return b + a;
+        break;
+    case '*':
+        return b * a;
+        break;
+    case '/':
+        return b / a;
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    int count = 0;
+    for (auto c : pref) {
+        count += 1;
+    }
+    for (int i = 0; i < count; ++i) {
+        if (pref[i] >= 48 && pref[i] <= 57) {
+            if (pref[i] == 48)
+                intstack.push(0);
+            if (pref[i] == 49)
+                intstack.push(1);
+            if (pref[i] == 50)
+                intstack.push(2);
+            if (pref[i] == 51)
+                intstack.push(3);
+            if (pref[i] == 52)
+                intstack.push(4);
+            if (pref[i] == 53)
+                intstack.push(5);
+            if (pref[i] == 54)
+                intstack.push(6);
+            if (pref[i] == 55)
+                intstack.push(7);
+            if (pref[i] == 56)
+                intstack.push(8);
+            if (pref[i] == 57)
+                intstack.push(9);
+        } else if (pref[i] >= 42 && pref[i] <= 47) {
+            char oper = pref[i];
+            int c = operat(oper);
+            intstack.push(c);
+        }
+    }
+    return intstack.peek();
 }
