adb6dca7cb323935957e29a51cef2a1422bc2f77
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-04-09 10:53:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,26 +4,25 @@
 
 template<typename T, int size>
 class TStack {
-  private:
-  T arr[size] = { 0 };
-  int top;
-public:
-  TStack() :top(-1){}
-  void push(T value) {
-    arr[++top] = value;
-  }
-  T get()const {
-    return arr[top];
-  }
-  T pop() {
-    return arr[top--];
-  }
-  bool isEmpty()const {
-    return top == -1;
-  }
-  bool isFull()const {
-    return top == size - 1;
-  }
+ private:
+    T * node;
+    int head;
+ public:
+    TStack() {
+        node = new T[size];
+        head = -1;
+    }
+    bool isEmpty() {
+        return head == -1;
+    }
+    void push(T a) {
+        node[++head] = a;
+    }
+    T peek() {
+        return node[head];
+    }
+    T pop() {
+        return node[head--];
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
